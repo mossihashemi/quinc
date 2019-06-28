bcbd11fdbad74136dff13332eb2ceb0b0191496e
mossiXYZ <mossi.hashemi@gmail.com>
2019-06-28 18:26:26 +0200

    288 Login-Logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,16 @@ import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-	
+
    constructor(translate: TranslateService) {
       translate.setDefaultLang('en');
       translate.addLangs(['en', 'fr']);
+
    }
-	
+
 }
--- src/app/shared/header/widgets/topbar/topbar.component.ts
+++ src/app/shared/header/widgets/topbar/topbar.component.ts
@@ -2,14 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../../../classes/product';
 import { WishlistService } from '../../../services/wishlist.service';
 import { Observable, of } from 'rxjs';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   selector: 'app-topbar',
   templateUrl: './topbar.component.html',
   styleUrls: ['./topbar.component.scss']
 })
-export class TopbarComponent implements OnInit {
-  
-  ngOnInit() { }
+export class TopbarComponent {
+
+  constructor(private afAuth: AngularFireAuth) {
+    afAuth.authState.subscribe(x => console.log(x));
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
 
 }
